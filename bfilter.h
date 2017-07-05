4930ef98c24a7fc255ab17370ae49b3394e4367c
wes <wjak56@gmail.com>
2017-07-05 18:17:05 -0400

    working setbit/unsetbit

--- bfilter.h
+++ bfilter.h
@@ -2,11 +2,13 @@ typedef
   struct {
     size_t num_elems;
     size_t num_ints;
-    int *arr;
+    uint32_t *arr;
   }
   bit_array_t;
 
-int printbits(bit_array_t*);
+int printbits(uint32_t, size_t);
+
+int print_barray(bit_array_t*);
 
 bit_array_t* new_bitarray(int);
 
